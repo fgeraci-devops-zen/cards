9ae733f236fd72bfea108807ad65c9d5e606ce0a
fgeraci-devops-zen <fgeraci@devops-zen.com>
2017-08-14 12:05:22 +0200

    more tests added

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,5 +1,18 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "Shiffling a deck randomize it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+    # avrei potuto asche scrivere refute deck == Cards.shuffle(deck)
+
+  end
+
   end
 
